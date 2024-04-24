615dfa367b01dc3576137202f9f7188b332dd5f7
cchrstna <ciciliachristina6@gmail.com>
2024-04-25 00:57:19 +0800

    progress

---
+++ controllers/controller.js
@@ -0,0 +1,11 @@
+class Controller {
+    static async getPage(req, res) {
+        try {
+            
+        } catch (error) {
+            res.send(error)
+        }
+    }
+}
+
+module.exports = Controller;
